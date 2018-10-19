bf0ae49ff361415844c456974d54c36a4018a4d2
David McCormick <dmccormick@starrez.com>
2018-10-19 20:56:27 +1100

    Include codemirror

--- src/StickyNote/StickyNote.tsx
+++ src/StickyNote/StickyNote.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
+import * as CodeMirror from 'react-codemirror';
 import * as ReactMarkdown from 'react-markdown';
+
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/markdown/markdown';
 import './StickyNote.css';
 
 class StickyNote extends React.Component {
     public render() {
         return (
-            <div className="sticky_note">
-                <ReactMarkdown source={"# test\n* test\n* test"} />
+            <div>
+                <div className="sticky_note">
+                    <ReactMarkdown source={"# test\n* test\n* test"} />
+                </div>
+                <div>
+                    <CodeMirror value={"# test\n* test\n* test"} onChange={undefined} options={{ mode: 'markdown', lineNumbers: true }} />
+                </div>
             </div>
         );
     }
